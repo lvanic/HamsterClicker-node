313428f21977479a446ec618b3d18e0a29b8c4fc
lvanic <polzavatelyoutuba@gmail.com>
2024-07-12 23:29:20 +0300

    ашкудишка

--- clicker-backend/src/sockets.js
+++ clicker-backend/src/sockets.js
@@ -197,10 +197,10 @@ export const registerEvents = (io) => {
   io.on("getBusinessesToBuy", async (userTgId) => {
     const user = await User.findOne({ tgId: userTgId });
     const businesses = await Business.find({ isDeleted: false });
-    const availableBusinesses =
-      user.businesses == null
-        ? businesses
-        : businesses.filter((b) => !user.businesses.includes(b._id));
+    const availableBusinesses = businesses.filter(
+      (b) => !user.businesses.some(userBusinessId => userBusinessId.equals(b._id))
+    );
+
     io.emit("businesses", availableBusinesses);
   });
 
