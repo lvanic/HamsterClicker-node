d9a6c1b11769c608e3975166aba2ec109f52ddcc
undeparg <undeparg@gmail.com>
2025-01-18 20:41:57 +0500

    ci: adapt eslint config for jest

--- clicker-backend/eslint.config.js
+++ clicker-backend/eslint.config.js
@@ -1,11 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import pluginJest from "eslint-plugin-jest";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest }
+    },
+    plugins: { jest: pluginJest },
+  },
   pluginJs.configs.recommended,
   eslintConfigPrettier
 ];
