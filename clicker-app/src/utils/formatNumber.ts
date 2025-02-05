d0aad9b58384fb587990b4492be2bfc518dcfc7c
undeparg <undeparg@gmail.com>
2025-02-06 03:02:31 +0500

    feat: add a second number after the point for the user's score

--- clicker-app/src/utils/formatNumber.ts
+++ clicker-app/src/utils/formatNumber.ts
@@ -1,10 +1,10 @@
 export function formatNumber(num: number) {
   if (num >= 1000000000) {
-    return `${(num / 1000000000).toFixed(1)}b`;
+    return `${(num / 1000000000).toFixed(2)}b`;
   } else if (num >= 1000000) {
-    return `${(num / 1000000).toFixed(1)}m`;
+    return `${(num / 1000000).toFixed(2)}m`;
   } else if (num >= 1000) {
-    return `${(num / 1000).toFixed(1)}k`;
+    return `${(num / 1000).toFixed(2)}k`;
   } else {
     return num.toFixed(0);
   }
